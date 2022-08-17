190952dc210caedc3d9590d1960283a9d312ff8b
Juansubas <juansubas@gmail.com>
2022-08-16 23:18:27 -0500

    Comentando Código

--- python/programacion funcional/ejercicio_zip.py
+++ python/programacion funcional/ejercicio_zip.py
@@ -13,6 +13,7 @@ caracteres_especiales = ['#','@','!','"','-','_','%','?','¿','=']
 relacion_letras_caracteres = list( zip(letras, caracteres_especiales))
 
 def codificar_mensaje(relacion_letras_caracteres: list, mensajes: str):
+#La función que se le pasa a filter debe retornar un booleano para el filtrar la lista
     filtrar = lambda letra: list(filter())
 
 print(relacion_letras_caracteres)
