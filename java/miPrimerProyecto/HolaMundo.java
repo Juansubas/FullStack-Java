c54c2f34bc0ff45cb3098d69277a87bacad541ec
Juansubas <juansubas@gmail.com>
2022-08-20 03:07:22 -0500

    Java Files

---
+++ java/miPrimerProyecto/HolaMundo.java
@@ -0,0 +1,11 @@
+// Package Engloba todos los archivos del directorio contenedor
+
+package miPrimerProyecto;
+
+//Importante el número de la clase debe eser el mismo del archivo
+public class HolaMundo{
+    public static void main(String[] args){
+        //Mostrar mensaje en consola
+        System.out.println("Hola Mundo");
+    }
+}
