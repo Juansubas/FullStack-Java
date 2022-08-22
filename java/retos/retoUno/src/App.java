c054eef3307e8af27f825385f2383426cb6f874d
Juansubas <juansubas@gmail.com>
2022-08-22 08:00:24 -0500

    Update

--- java/retos/retoUno/src/App.java
+++ java/retos/retoUno/src/App.java
@@ -3,7 +3,7 @@ public class App {
         BecaUniversitaria becaUniversitaria = new BecaUniversitaria();
         System.out.println(becaUniversitaria.calcularInteresSimple());
         System.out.println(becaUniversitaria.calcularInteresCompuesto());
-        System.out.println(becaUniversitaria.compararInversion(60,13000,1.4));
+        System.out.println(becaUniversitaria.compararInversion(60,14000,1.4));
         System.out.println(becaUniversitaria.compararInversion(48,10000,2.0));
         System.out.println(becaUniversitaria.compararInversion());
     }
